dfee4f3a7dd7049b880f3af87af5a9bee55f44a7
Ahmed-Mokhtar717 <ahmed734717285@gmail.com>
2025-06-07 02:22:08 +0300

    Initial Commit

---
+++ src/main/java/com/ahmed/chatmespringboot/ChatMeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ahmed.chatmespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatMeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatMeSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahmed/chatmespringboot/ChatMeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmed.chatmespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatMeSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
