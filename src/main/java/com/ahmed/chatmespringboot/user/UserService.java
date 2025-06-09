ae288af7ab5f6279f03121f7c62986acf59b1824
Ahmed-Mokhtar717 <ahmed734717285@gmail.com>
2025-06-10 01:16:32 +0300

    Creating the user service and repository

---
+++ src/main/java/com/ahmed/chatmespringboot/user/UserService.java
@@ -0,0 +1,31 @@
+package com.ahmed.chatmespringboot.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository repository;
+
+    public void saveuser(User user) {
+        user.setStatus(Status.ONLINE);
+        repository.save(user);
+    }
+
+    public void disconnect(User user) {
+        var storedUser = repository.findById(user.getNickname())
+                .orElse(null);
+        if (storedUser != null) {
+            storedUser.setStatus(Status.OFFLINE);
+            repository.save(storedUser);
+        }
+    }
+
+    public List<User> findConnectedUsers(){
+        return repository.findAllByStatus(Status.ONLINE);
+    }
+}
